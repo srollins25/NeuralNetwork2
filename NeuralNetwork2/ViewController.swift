b272484d02d361ba5a3c05d0720e8c9c4366834f
stephan rollins <stephan.rollins@yahoo.com>
2018-12-23 18:06:07 -0800

    Initial Commit

---
+++ NeuralNetwork2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeuralNetwork2
+//
+//  Created by stephan rollins on 12/23/18.
+//  Copyright © 2018 stephan rollins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
